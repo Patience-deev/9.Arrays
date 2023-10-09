aea7c7a86c206a38db657ff06514baa05b85b209
Dell-User <Dell-User@DESKTOP-78SJPVI>
2023-10-09 13:40:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //OneWayAttribute way of creating an array
+            int[] luckynumbers = { 4, 5, 8, 9, };
+            Console.WriteLine(luckynumbers[2]);
+            //if you want to populate it later, the 5 is the size
+            string[] friends = new string[5];
+            friends[0] = "Patience";
+            friends[1] = "Charles";
+            friends[2] = "Sue";
+            Console.WriteLine(friends[2]);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
